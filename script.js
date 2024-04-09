2f3340a04192f7d1889fe8dcca4d897e8fa65715
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-04-09 19:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// your code here
+const nameInput = document.querySelector("#name");
+const h3 = document.querySelector("#year");
+const submitBtn =  document.querySelector("#submit");
+
+submitBtn.addEventListener("click", () => {
+    const name = nameInput.value;
+    h3.innerHTML = name; 
+});
