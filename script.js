dc67a499c08ed0d062e8b10021c7898f47cd5c4f
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-04-11 11:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const nameInput = document.querySelector("#name");
 const yearInput = document.querySelector("#year");
 const h3 = document.querySelector("#url");
-const submitBtn =  document.querySelector("#submit");
+const submitBtn = document.querySelector("#submit");
 
 submitBtn.addEventListener("click", () => {
-    const name = nameInput.value;
-    const year = nameInput.value;
-	alert(h3.innerHTML)
-	if(name !== "" && year !== ""){
-		h3.innerHTML = `${h3.innerHTML}`
-	} 
+  const name = nameInput.value;
+  const year = yearInput.value;
+  if (name !== "" && year !== "") {
+    h3.innerHTML = `${h3.innerHTML}?name=${name}&year=${year}`;
+    nameInput.value = "";
+    yearInput.value = "";
+  } else if (name && !year) {
+    h3.innerHTML = `${h3.innerHTML}?name=${name}`;
+    nameInput.value = "";
+    yearInput.value = "";
+  } else if (!name && year) {
+    h3.innerHTML = `${h3.innerHTML}?year=${year}`;
+    nameInput.vavaluel = "";
+    yearInput.value = "";
+  }
 });
